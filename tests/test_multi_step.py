f6dc5c79b59d8dc745cd63d10ffca0bc4c08e544
Dennis Feng <v-dennisfeng@microsoft.com>
2025-05-27 11:57:33 -0700

    moved reusable fixtures to confetest

--- tests/test_multi_step.py
+++ tests/test_multi_step.py
@@ -1,25 +1,7 @@
 """Unit tests for multi-step evals"""
 
 from tool_usage_evals.multi_step import run_agent_turn, AgentTurnResult
-import os
-from azure.identity import DefaultAzureCredential, get_bearer_token_provider
-import pytest
 from openai import AzureOpenAI
-from dotenv import load_dotenv
-
-load_dotenv()
-
-
-@pytest.fixture(scope="session")
-def aoai_client() -> AzureOpenAI:
-    """Azure OpenAI client"""
-    token_provider = get_bearer_token_provider(DefaultAzureCredential(), "https://cognitiveservices.azure.com/.default")
-    client = AzureOpenAI(
-        azure_ad_token_provider=token_provider,
-        azure_endpoint=os.environ["AOAI_ENDPOINT"],
-        api_version=os.environ["AOAI_API_VERSION"],
-    )
-    return client
 
 
 def mock_get_weather(location: str) -> str:
